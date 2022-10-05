e4b6722921b68f5252d9086ff51dea1b94c71b81
Ulad Kasach <uladkasach@gmail.com>
2022-10-05 10:23:54 -0400

    fix(exports): expose the castCookiesToString method

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { exposeCookieFromReq } from './exposeCookieFromReq';
 export { getCookie } from './getCookie';
 export { setCookie } from './setCookie';
 export { getCookiesFromString } from './parse/getCookiesFromString';
+export { castCookiesToString } from './parse/castCookiesToString';
 export { Cookie } from './domain/Cookie';
