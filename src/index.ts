cab40a21299e29180b6924dca69b5d4f970cd830
Ulad Kasach <uladkasach@gmail.com>
2022-10-05 10:21:50 -0400

    feat(exports): expose Cookie domain object

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { exposeCookieFromReq } from './exposeCookieFromReq';
 export { getCookie } from './getCookie';
 export { setCookie } from './setCookie';
 export { getCookiesFromString } from './parse/getCookiesFromString';
+export { Cookie } from './domain/Cookie';
