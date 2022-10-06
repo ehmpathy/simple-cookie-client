738f48b88b7e50876bc1a5a83c52a1d5e67b9a84
Ulad Kasach <uladkasach@gmail.com>
2022-10-06 07:14:52 -0400

    fix(tests): await getCookie in unit test

--- src/getCookie.test.ts
+++ src/getCookie.test.ts
@@ -11,40 +11,40 @@ const getExposedCookieMock = getInMemoryCookie as jest.Mock;
 describe('getCookie', () => {
   beforeEach(() => jest.clearAllMocks());
   describe('storage mechanism AUTO', () => {
-    it('should get the cookie from the document store, if its defined in the document store', () => {
+    it('should get the cookie from the document store, if its defined in the document store', async () => {
       // mock the cookie is defined
       const cookieToFind = new Cookie({ name: 'authorization', value: '821' });
       getDocumentCookieMock.mockReturnValueOnce(cookieToFind); // mock that it is exposed
 
       // get the cookie
-      const cookie = getCookie({ name: 'authorization' });
+      const cookie = await getCookie({ name: 'authorization' });
 
       // check expected results
       expect(cookie).toEqual(cookieToFind);
       expect(getDocumentCookieMock).toHaveBeenCalledTimes(1);
       expect(getExposedCookieMock).not.toHaveBeenCalled();
     });
-    it('should get the cookie from the exposed store, if it is not in the document store and is in the exposed store', () => {
+    it('should get the cookie from the exposed store, if it is not in the document store and is in the exposed store', async () => {
       // mock the cookie is defined
       const cookieToFind = new Cookie({ name: 'authorization', value: '821' });
       getDocumentCookieMock.mockReturnValueOnce(null);
       getExposedCookieMock.mockReturnValueOnce(cookieToFind);
 
       // get the cookie
-      const cookie = getCookie({ name: 'authorization' });
+      const cookie = await getCookie({ name: 'authorization' });
 
       // check expected results
       expect(cookie).toEqual(cookieToFind);
       expect(getDocumentCookieMock).toHaveBeenCalledTimes(1);
       expect(getExposedCookieMock).toHaveBeenCalledTimes(1);
     });
-    it('should gracefully return null if cookie is not found in either store', () => {
+    it('should gracefully return null if cookie is not found in either store', async () => {
       // mock the cookie is not defined
       getDocumentCookieMock.mockReturnValueOnce(null);
       getExposedCookieMock.mockReturnValueOnce(null);
 
       // get the cookie
-      const cookie = getCookie({ name: 'authorization' });
+      const cookie = await getCookie({ name: 'authorization' });
 
       // check expected results
       expect(cookie).toEqual(null);
